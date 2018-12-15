915fb00619365fd4b9142276b3b92001fce183d9
Robbie Greiner <robbieg@gmail.com>
2018-12-15 11:56:23 -0800

    added board class and valid_coordinate?

---
+++ lib/board.rb
@@ -0,0 +1,28 @@
+require './cell'
+
+class Board
+  def initialize
+    @cells = {
+      "A1" => Cell.new("A1"),
+      "A2" => Cell.new("A2"),
+      "A3" => Cell.new("A3"),
+      "A4" => Cell.new("A4"),
+      "B1" => Cell.new("B1"),
+      "B2" => Cell.new("B2"),
+      "B3" => Cell.new("B3"),
+      "B4" => Cell.new("B4"),
+      "C1" => Cell.new("C1"),
+      "C2" => Cell.new("C2"),
+      "C3" => Cell.new("C3"),
+      "C4" => Cell.new("C4")
+    }
+  end
+
+  def valid_coordinate?(coord)
+    @cells[coord] != nil
+  end
+
+  def cells
+    @cells
+  end
+end
